49a6c7f56d2b90a3996b36570501f28e4744415c
Juliano Santos <julianosantosdev@outlook.com>
2023-08-01 23:55:34 -0300

    feat: cors

--- src/app.ts
+++ src/app.ts
@@ -5,9 +5,15 @@ import usersRoute from './routes/users.routes';
 import { handleErrors } from './errors';
 import loginRoute from './routes/login.routes';
 import contactRoute from './routes/contact.routes';
+import cors from 'cors';
 
 const app: Application = express();
 app.use(express.json());
+app.use(
+  cors({
+    origin: 'http://localhost:5173',
+  })
+);
 
 app.use('/users', usersRoute);
 app.use('/login', loginRoute);
